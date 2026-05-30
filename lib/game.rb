3490f5b5e41e88a7394934341170604ebcacdeb2
Dzbice <dechawnz@gmail.com>
2026-05-30 01:45:21 -0400

    base hangman done

--- lib/game.rb
+++ lib/game.rb
@@ -5,19 +5,31 @@ require_relative 'hangman'
 class Game
   attr_accessor :hangman
 
-  def initialize(word = '', guesses = [], path = 'resources/google-10000-english-no-swears.txt')
+  def initialize(path = 'resources/google-10000-english-no-swears.txt')
     @path = path
-    @hangman = Hangman.new(word, guesses)
-    return unless @hangman.word.empty?
-
-    @hangman.word = get_word(path)
+    word = get_word(path)
+    @hangman = Hangman.new(word)
   end
 
   def get_word(path)
     words = File.readlines(path)
     loop do
       random_index = rand(words.length)
-      return words[random_index] if words[random_index].length >= 5 && words[random_index].length <= 12
+      return words[random_index].chomp if words[random_index].length >= 5 && words[random_index].length <= 12
+    end
+  end
+
+  def main_loop
+    loop do
+      break if @hangman.fail?
+      break if @hangman.complete?
+
+      puts @hangman
+      puts 'enter a letter'
+      input = gets.chomp
+      next unless input =~ /[a-zA-z]/ && input.size == 1
+
+      @hangman.right?(input)
     end
   end
 end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,52 @@
 # Represents our hangman
 class Hangman
-  attr_accessor :word, :guesses
+  attr_accessor :word, :guesses, :correct, :incorrect
 
-  def initialize(word = '', guesses = [])
+  def initialize(word)
     @word = word
-    @guesses = guesses
+    @guesses = []
+    @correct = Array.new(@word.size, '_')
+    @incorrect = 0
+  end
+
+  def complete?
+    return unless @correct.join == @word
+
+    puts "Good job! The word was #{@word}"
+    true
+  end
+
+  def right?(letter)
+    if @word.include?(letter)
+      puts 'Correct'
+      update_correct(letter)
+    else
+      puts 'Incorrect'
+      @incorrect += 1
+    end
+    add_guess(letter)
+  end
+
+  def update_correct(letter)
+    indexs = (0...@word.length).find_all { |i| word[i, 1] == letter.downcase }
+    indexs.each { |x| @correct[x] = @word[x] }
+  end
+
+  def fail?
+    return unless incorrect >= 6
+
+    puts "fail, you dead, the word was #{@word}"
+    true
+  end
+
+  def add_guess(input)
+    @guesses.push(input)
+  end
+
+  def to_s
+    "Size: #{@word.length}\n" \
+    "Guesses: #{@guesses.join(',')}\n" \
+    "Incorrects: #{@incorrect}/6\n" \
+    "#{@correct.join}"
   end
 end
